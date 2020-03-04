aee5bc8fe6ccf36eae4c9db2d96f6cbf26acc9d7
Chee Yeo <ckyeo.1@gmail.com>
2020-03-04 12:16:31 +0000

    Updates to inheritance examples

--- inhertitance/case_study/assignmentgrader.py
+++ inhertitance/case_study/assignmentgrader.py
@@ -1,3 +1,18 @@
 class AssignmentGrader:
 	def __init__(self, student, AssignmentClass):
-		pass
+		self.assignment = AssignmentClass()
+		# Assigns student attr
+		self.assignment.student  = student
+		self.attempts = 0
+		self.correct_attempts = 0
+
+	def check(self, code):
+		self.attempts += 1
+		result = self.assignment.check(code)
+		if result:
+			self.correct_attempts += 1
+
+		return result
+
+	def lesson(self):
+		return self.assignment.lesson()
--- inhertitance/case_study/grader.py
+++ inhertitance/case_study/grader.py
@@ -1,24 +1,6 @@
 import uuid
 from assignment import Assignment
-
-class AssignmentGrader:
-	def __init__(self, student, AssignmentClass):
-		self.assignment = AssignmentClass()
-		# Assigns student attr
-		self.assignment.student  = student
-		self.attempts = 0
-		self.correct_attempts = 0
-
-	def check(self, code):
-		self.attempts += 1
-		result = self.assignment.check(code)
-		if result:
-			self.correct_attempts += 1
-
-		return result
-
-	def lesson(self):
-		return self.assignment.lesson()
+from assignmentgrader import AssignmentGrader
 
 class Grader:
 	def __init__(self):
